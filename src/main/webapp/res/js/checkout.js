a9faf64b77e99efcd6806d90617205a1ce27b131
VictorZ <zqf2887@hotmail.com>
2014-09-18 00:17:55 +1000

    move controller to separate file

--- src/main/webapp/res/js/checkout.js
+++ src/main/webapp/res/js/checkout.js
@@ -1,4 +1,4 @@
-var checkoutApp = angular.module('checkoutApp', []);
+var checkoutApp = angular.module('checkoutApp', ['checkout.controller']);
 
 checkoutApp.value('moduleStatus', {
 	INITIAL: "initial",
@@ -14,53 +14,4 @@ checkoutApp.directive('nameDetailsModule', function(){
     };
 });
 
-checkoutApp.controller('ModuleController',['$scope', 'moduleStatus', function($scope, moduleStatus){
-	$scope.currentStatus = moduleStatus.INITIAL;
 
-	$scope.formData = {};
-	$scope.master = {};
-
-	$scope.module = {
-		title: "Name Details",
-		header: "Please enter your name and date of birth",
-		summaryHeader: "Your name details"
-	};
-
-	$scope.isInitial = function(){
-		return $scope.currentStatus === moduleStatus.INITIAL;
-		//return false;
-	};
-	
-	$scope.isOpen = function(){
-		return $scope.currentStatus === moduleStatus.OPEN;
-	};
-	
-	$scope.isEdit = function(){
-		return $scope.currentStatus === moduleStatus.EDIT; 
-		//return true;
-	};
-	
-	$scope.isSaved = function(){
-		return $scope.currentStatus === moduleStatus.SAVED;
-		//return false;
-	};
-	
-	$scope.open = function() {
-		$scope.currentStatus = moduleStatus.OPEN;
-	};
-	
-	$scope.save = function(formData) {
-		$scope.master = angular.copy(formData);
-		$scope.currentStatus = moduleStatus.SAVED;
-	};
-	
-	$scope.edit = function() {
-		$scope.currentStatus = moduleStatus.EDIT;
-	};
-	
-	$scope.cancel = function() {
-		$scope.formData = angular.copy($scope.master);
-		$scope.currentStatus = moduleStatus.SAVED;
-	};
-	
-}]);
---
+++ src/main/webapp/res/js/moduleController.js
@@ -0,0 +1,51 @@
+angular.module('checkout.controller',[])
+.controller('ModuleController',['$scope', 'moduleStatus', function($scope, moduleStatus){
+	$scope.currentStatus = moduleStatus.INITIAL;
+
+	$scope.formData = {};
+	$scope.master = {};
+
+	$scope.module = {
+		title: "Name Details",
+		header: "Please enter your name and date of birth",
+		summaryHeader: "Your name details"
+	};
+
+	$scope.isInitial = function(){
+		return $scope.currentStatus === moduleStatus.INITIAL;
+		//return false;
+	};
+
+	$scope.isOpen = function(){
+		return $scope.currentStatus === moduleStatus.OPEN;
+	};
+
+	$scope.isEdit = function(){
+		return $scope.currentStatus === moduleStatus.EDIT;
+		//return true;
+	};
+
+	$scope.isSaved = function(){
+		return $scope.currentStatus === moduleStatus.SAVED;
+		//return false;
+	};
+
+	$scope.open = function() {
+		$scope.currentStatus = moduleStatus.OPEN;
+	};
+
+	$scope.save = function(formData) {
+		$scope.master = angular.copy(formData);
+		$scope.currentStatus = moduleStatus.SAVED;
+	};
+
+	$scope.edit = function() {
+		$scope.currentStatus = moduleStatus.EDIT;
+	};
+
+	$scope.cancel = function() {
+		$scope.formData = angular.copy($scope.master);
+		$scope.currentStatus = moduleStatus.SAVED;
+	};
+
+}]);
